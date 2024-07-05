363a4eee80b7b784a3aac8cb8c03e5569066d564
usamaJ17 <usamajalal17@gmail.com>
2024-07-05 19:57:37 +0500

    test

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,7 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
+        dd("asdas");
         return $request->expectsJson() ? null : route('login');
     }
 }
