4525555f0cb9e644ccdae5a11750fa1b43a49817
usamaJ17 <usamajalal17@gmail.com>
2024-07-21 18:53:46 +0500

    upd

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/sym', function () {
     Artisan::call('route:clear');
     Artisan::call('config:clear');
