cc72d4a82a2a21b5c5dd2a1b3e3ce614fe30fe61
usamaJ17 <usamajalal17@gmail.com>
2024-07-21 19:07:42 +0500

    upd

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\dashboardController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\SignalingController;
 use Illuminate\Support\Facades\Artisan;
@@ -13,65 +13,23 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
 |
 */
 
 Route::get('/sym', function () {
-    Artisan::call('route:clear');
-    Artisan::call('config:clear');
-    Artisan::call('cache:clear');
-    Artisan::call('view:clear');
-    Artisan::call('optimize:clear');
     Artisan::call('storage:link');
-    Artisan::call('config:cache');
-    Artisan::call('route:cache');
-    Artisan::call('view:cache');
     return 'success';
 });
-Route::get('/pay', function () {
-    $curl = curl_init();
-  
-    curl_setopt_array($curl, array(
-      CURLOPT_URL => "https://api.paystack.co/transaction/verify/5port7teby",
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_ENCODING => "",
-      CURLOPT_MAXREDIRS => 10,
-      CURLOPT_TIMEOUT => 30,
-      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-      CURLOPT_CUSTOMREQUEST => "GET",
-      CURLOPT_HTTPHEADER => array(
-        "Authorization: Bearer ".env('PAYSTACK_SECRET_KEY'),
-        "Cache-Control: no-cache",
-      ),
-    ));
-    
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
-  
-    curl_close($curl);
-    
-    if ($err) {
-      echo "cURL Error #:" . $err;
-    } else {
-        dd($response);
-    }
-   
-});
-// Laravel 8 & 9
-Route::post('/pay', [PaymentController::class, 'redirectToGateway'])->name('pay');
-Route::get('/payment/callback', [PaymentController::class, 'handleGatewayCallback'])->name('callback');
 
-Route::any('crypto_call', [PaymentController::class, 'crypto_call'])->name('crypto_call');
-
-Route::get('/',function(){
-  dd("Welcome to the home page");
+Route::get('/', function () {
+    return redirect()->route('dashboard');
 })->name('home');
-Route::get('/login',[dashboardController::class,'login'])->name('login.form');
-Route::post('/login',[AuthController::class,'adminLogin'])->name('login');
 
-// Route::group(['prefix' => 'dashboard'], function () {
-//   Route::get('/',[dashboardController::class,'index'])->name('dashboard'); 
-// });
-Route::get('dashboard',[dashboardController::class,'index'])->name('dashboard'); 
+Route::get('/login', [DashboardController::class, 'login'])->name('login.form');
+Route::post('/login', [AuthController::class, 'adminLogin'])->name('login');
+
+Route::middleware(['adminCheck'])->prefix('portal')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+});
