d2b75036d45b2104ed58570050db6d3190d5f70b
Waheed Ahmed <waheed.gujjar.092@gmail.com>
2024-12-12 22:48:42 +0500

    Edit personal dynamic files saved

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -281,10 +281,21 @@ class AuthController extends Controller
     }
     public function savePersonalDetails(Request $request){
         $user = User::find(Auth::user()->id);
-        if(null !== $request->file('profile_image')){
-            $user->clearMediaCollection();
-            $user->addMedia($request->file('profile_image'))->toMediaCollection();
-        }
+        // if(null !== $request->file('profile_image')){
+        //     $user->clearMediaCollection();
+        //     $user->addMedia($request->file('profile_image'))->toMediaCollection();
+        // }
+
+        $fileFields = array_keys($request->files->all());
+
+        foreach ($fileFields as $field) {
+            if ($request->hasFile($field)) {
+                $pro->clearMediaCollection($field);
+                $pro->addMedia($request->file($field))->toMediaCollection($field);
+            }
+        } 
+
+        
         // remove email and password from request if present
         unset($request['email']);
         unset($request['password']);
