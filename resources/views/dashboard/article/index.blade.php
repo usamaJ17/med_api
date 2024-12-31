830c641fd2a5902329dd5c7d706d536e073cc6c2
Waheed Ahmed <waheed.gujjar.092@gmail.com>
2024-12-31 16:05:55 +0500

    error

--- resources/views/dashboard/article/index.blade.php
+++ resources/views/dashboard/article/index.blade.php
@@ -216,7 +216,7 @@
             
             function deleteArticle(name) {
                 $.ajax({
-                    url : "{{ url('articles/delete') }}/" + name,
+                    url : "{{ url('portal/articles/delete') }}/" + name,
                     type: 'DELETE',
                     data: {
                         _token: "{{ csrf_token() }}"
