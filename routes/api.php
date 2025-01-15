b780b4a1a75d2e53836ff6229c3f7996ebec5ec7
usamaJ17 <usamajalal17@gmail.com>
2025-01-15 18:58:01 +0500

    Updated changes

--- routes/api.php
+++ routes/api.php
@@ -72,8 +72,7 @@ Route::middleware(['cros'])->group(function () {
 });
 
 
-//Route::middleware(['auth:sanctum', 'cros'])->group(function () {
-Route::middleware(['cros'])->group(function () {
+Route::middleware(['auth:sanctum', 'cros'])->group(function () {
   Route::post('logout', [AuthController::class, 'logout']);
   Route::post('device_token', [AuthController::class, 'saveDeviceToken']);
   Route::post('app_feedback', [HelperController::class, 'saveUserFeedback']);
