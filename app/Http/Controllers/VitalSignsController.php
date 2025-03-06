c2cde888edf4a78956aa769ae5a6cac68d707d6a
usamaJ17 <usamajalal17@gmail.com>
2025-03-07 00:59:53 +0500

    updated PDFs

--- app/Http/Controllers/VitalSignsController.php
+++ app/Http/Controllers/VitalSignsController.php
@@ -2,19 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\GenerateAppointmentSummaryPDF;
 use App\Models\ClinicalNotes;
 use App\Models\ClinicalNotesCustomField;
 use App\Models\ClinicalNotesField;
 use App\Models\DynamicFiled;
 use App\Models\NotesComment;
-use App\Models\SummaryDynamicField;
 use App\Models\VitalSigns;
-use App\Models\ProfessionalDetails;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Schema;
-use PDF;
 
 class VitalSignsController extends Controller
 {
@@ -78,74 +74,16 @@ class VitalSignsController extends Controller
 
 
     public function saveNotes(Request $request){
-        $summary = $request->note;
-        $summary = str_replace("'", '"', $summary);
-        $summaryJson = json_decode($summary);
-        $professionDetail = User::whereHas("roles", function($q){ $q->where("name", "medical"); })->with('professionalDetails')->where('id', auth()->user()->id)->first();
-        //print_r($professionDetail);
-        $patientDetail = User::with('medicalDetails')->where('id', $request->user_id)->first();
-        //  return response()->json([
-        //         'status' => 200,
-        //         'professionDetail'=> $professionDetail,
-        //     ], 200);
-        $pdfs_list = array();
-        foreach ($summaryJson as $key => $value) { 
-            $weight = isset($patientDetail->medicalDetails->weight) ? $patientDetail->medicalDetails->weight . ' kg' : 'N/A';    
-            $registrationNumber = $professionDetail->professionalDetails->regestraion_number ?? 'N/A';
-           
-            if ($registrationNumber !== 'N/A') {
-                $maskedRegistrationNumber = '****' . substr($registrationNumber, -2);
-            } else {
-                $maskedRegistrationNumber = 'N/A';
-            }       
-            
-            if ($key === 'Prescription' || $key === 'prescription') {
-                $background = public_path('pdfs/background.png');
-            }
-            else{
-                $background = public_path('pdfs/background2.png');
-            }
-            $data = [
-                'background' => $background,
-                'date' => date('d/m/Y'),
-                'patient_id' => $patientDetail['id'] ?? 'N/A',
-                'patient_name' => $patientDetail['first_name'] . ' ' . $patientDetail['last_name'],
-                'patient_address' => $patientDetail['city'] . ', ' . $patientDetail['state'] . ', ' . $patientDetail['country'],
-                'patient_phone' => $patientDetail['contact'] ?? 'N/A',
-                'patient_age' => isset($patientDetail['dob']) ? date_diff(date_create($patientDetail['dob']), date_create('today'))->y . ' years' : 'N/A',
-                'patient_weight' => $weight,
-                'patient_gender' => $patientDetail['gender'] ?? '',
-                'doctor_name' => $professionDetail['first_name'] . ' ' . $professionDetail['last_name'],
-                'doctor_license' => $maskedRegistrationNumber,
-                'doctor_signature' => $professionDetail->professionalDetails->signature ?? 'N/A',
-                'note_key' => $key,
-                'note_value' => $value,
-            ];
-            if ($key === 'Prescription' || $key === 'prescription') {
-                $prefix = 'prescription_';
-                $pdf = Pdf::loadView('pdf.prescription', $data)->setPaper('legal', 'portrait');
-            } else {
-                $prefix = 'none_prescription_';
-                $pdf = Pdf::loadView('pdf.consultation_summary', $data)->setPaper('legal', 'portrait');
-            }
-            $file_name = 'prescriptions/'. $prefix . time() . '.pdf';
-            if (!is_dir(public_path('prescriptions'))) {
-                mkdir(public_path('prescriptions'), 0777, true);
-            }
-            $filePath = public_path($file_name);
-            $pdf->save($filePath);
-            $pdfs_list[$value] =  $file_name;
-        }
-
+        $summaryJson = json_decode(str_replace("'", '"', $request->note));
         $request->merge([
             'created_by' => auth()->user()->id,
-            'note' => json_encode($summaryJson),
-            'pdfs_list' => json_encode($pdfs_list)
+            'note' => json_encode($summaryJson)
         ]);
         $clinicalNote = ClinicalNotes::create($request->all());
+        GenerateAppointmentSummaryPDF::dispatch($clinicalNote->id , $summaryJson , $request->user_id , auth()->user()->id);
         return response()->json([
             'status' => 200,
-            'message'=> 'Notes Created Successfully...',
+            'message'=> 'Notes Created Successfully, PDF will be available in a while',
         ], 200);
     }
     public function saveComment(Request $request){
---
+++ app/Jobs/GenerateAppointmentSummaryPDF.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\ClinicalNotes;
+use App\Models\User;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Barryvdh\DomPDF\PDF;
+
+class GenerateAppointmentSummaryPDF implements ShouldQueue
+{
+    use Queueable;
+    private $summaryJson;
+    private $patientId;
+    private $professionalId;
+    private $clinicalNoteId;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($clinicalNoteId , $summaryJson , $patientId , $professionalId)
+    {
+        $this->clinicalNoteId = $clinicalNoteId;
+        $this->summaryJson = $summaryJson;
+        $this->patientId = $patientId;
+        $this->professionalId = $professionalId;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $pdfs_list = array();
+        $professionDetail = User::whereHas("roles", function($q){ $q->where("name", "medical"); })->with('professionalDetails')->where('id', $this->professionalId)->first();
+        $patientDetail = User::with('medicalDetails')->where('id', $this->patientId)->first();
+        foreach ($this->summaryJson as $key => $value) {
+            $weight = isset($patientDetail->medicalDetails->weight) ? $patientDetail->medicalDetails->weight . ' kg' : 'N/A';
+            $registrationNumber = $professionDetail->professionalDetails->regestraion_number ?? 'N/A';
+
+            if ($registrationNumber !== 'N/A') {
+                $maskedRegistrationNumber = '****' . substr($registrationNumber, -2);
+            } else {
+                $maskedRegistrationNumber = 'N/A';
+            }
+
+            if ($key === 'Prescription' || $key === 'prescription') {
+                $background = public_path('pdfs/background.png');
+            }
+            else{
+                $background = public_path('pdfs/background2.png');
+            }
+            $data = [
+                'background' => $background,
+                'date' => date('d/m/Y'),
+                'patient_id' => $patientDetail['id'] ?? 'N/A',
+                'patient_name' => $patientDetail['first_name'] . ' ' . $patientDetail['last_name'],
+                'patient_address' => $patientDetail['city'] . ', ' . $patientDetail['state'] . ', ' . $patientDetail['country'],
+                'patient_phone' => $patientDetail['contact'] ?? 'N/A',
+                'patient_age' => isset($patientDetail['dob']) ? date_diff(date_create($patientDetail['dob']), date_create('today'))->y . ' years' : 'N/A',
+                'patient_weight' => $weight,
+                'patient_gender' => $patientDetail['gender'] ?? '',
+                'doctor_name' => $professionDetail['first_name'] . ' ' . $professionDetail['last_name'],
+                'doctor_license' => $maskedRegistrationNumber,
+                'doctor_signature' => $professionDetail->professionalDetails->signature ?? 'N/A',
+                'note_key' => $key,
+                'note_value' => $value,
+            ];
+            if ($key === 'Prescription' || $key === 'prescription') {
+                $prefix = 'prescription_';
+                $pdf = Pdf::loadView('pdf.prescription', $data)->setPaper('legal', 'portrait');
+            } else {
+                $prefix = 'none_prescription_';
+                $pdf = Pdf::loadView('pdf.consultation_summary', $data)->setPaper('legal', 'portrait');
+            }
+            $file_name = 'prescriptions/'. $prefix . time() . '.pdf';
+            if (!is_dir(storage_path('prescriptions'))) {
+                mkdir(storage_path('prescriptions'), 0777, true);
+            }
+            $filePath = storage_path($file_name);
+            $pdf->save($filePath);
+            $pdfs_list[$value] =  $file_name;
+        }
+        $clinicalNote = ClinicalNotes::find($this->clinicalNoteId);
+        $clinicalNote->update([
+            'pdfs_list' => json_encode($pdfs_list)
+        ]);
+    }
+}
