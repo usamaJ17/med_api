2e69150186be18f2fe622e9e89be1b9b48ea65d3
usamaJ17 <usamajalal17@gmail.com>
2025-05-31 17:51:25 +0500

    updated broadcast

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -15,9 +15,9 @@ use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 
-class MessageSent implements ShouldBroadcast
+class MessageSent implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
