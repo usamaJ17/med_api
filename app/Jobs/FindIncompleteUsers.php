f59f4c7a653c158e928f797ba2f86dd9194a968f
usamaJ17 <usamajalal17@gmail.com>
2025-07-05 21:59:44 +0500

    upd

--- app/Jobs/FindIncompleteUsers.php
+++ app/Jobs/FindIncompleteUsers.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Support\Facades\Log;
 
 class FindIncompleteUsers implements ShouldQueue
 {
@@ -40,25 +41,32 @@ class FindIncompleteUsers implements ShouldQueue
                 ->orderByDesc('scheduled_at')
                 ->first();
 
+            Log::info('Processing user: ' . $user->id . ' - ' . $email);
+
             $nextTime = null;
 
             if (!$lastScheduled) {
+                Log::info('Initial scheduling for user: ' . $user->id . ' - ' . $email);
                 $verifiedAt = Carbon::parse($user->email_verified_at);
                 $diff = $now->diffInHours($verifiedAt);
 
                 if ($diff >= 24) {
+                    Log::info('DIFF IS 24 for user: ' . $user->id . ' - ' . $email);
                     $nextTime = $now->copy();
                 }
             } else {
+                Log::info('IN ELSE for user: ' . $user->id . ' - ' . $email . ' is ' . $lastScheduled->scheduled_at);
                 $last = Carbon::parse($lastScheduled->scheduled_at);
                 $diff = $now->diffInHours($last);
 
                 if ($diff >= 72) {
+                    Log::info('DIFF IS 72 for user: ' . $user->id . ' - ' . $email);
                     $nextTime = $now->copy();
                 }
             }
 
             if ($nextTime) {
+                Log::info('IN nextTime for user: ' . $user->id . ' - ' . $email . ' at ' . $nextTime);
                 EmailNotifications::create([
                     'email' => $email,
                     'name' => $user->name,
