5078f25911a4e04a34237d5f341c1f911d989fba
usamaJ17 <usamajalal17@gmail.com>
2025-07-25 02:15:33 +0500

    upd

--- app/Models/EmailNotifications.php
+++ app/Models/EmailNotifications.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class EmailNotifications extends Model
 {
     protected $table = 'email_notifications';
+    protected $casts = [
+        'scheduled_at' => 'datetime',
+        'sent_at' => 'datetime',
+    ];
     protected $fillable = ['email', 'name', 'type', 'status', 'sent_at', 'scheduled_at'];
 }
