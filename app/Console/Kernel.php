c8cf3052921d0ccd3157bb1652727827938e259b
usamaJ17 <usamajalal17@gmail.com>
2025-07-25 01:43:11 +0500

    upd

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,12 +19,10 @@ class Kernel extends ConsoleKernel
     {
         $schedule->job(new UpdateChatBoxStatus)->everyTenMinutes();
         $schedule->job(new SendBulkPushNotification)->everyMinute();
-        $schedule->command('queue:work --sleep=3 --tries=3 --stop-when-empty')
-            ->everyMinute()
-            ->withoutOverlapping();
-        $schedule->job(new DeleteUnverifiedUsers)->everyTwoHours();
-        $schedule->job(new SendEmailNotifications)->everyHour();
-        $schedule->job(new FindIncompleteUsers)->everyTwoHours();
+        $schedule->command('queue:work --sleep=3 --tries=3 --stop-when-empty')->everyMinute()->withoutOverlapping();
+        $schedule->job(new DeleteUnverifiedUsers)->everyTwoMinutes();
+        $schedule->job(new SendEmailNotifications)->everyTwoMinutes();
+        $schedule->job(new FindIncompleteUsers)->everyTwoMinutes();
     }
 
     /**
